1c4d819f4287ec00c75f2cbe35ee3c59091d9fa5
hibasaadoud <hibasaadoud235@gmail.com>
2025-08-29 09:51:31 -0700

    Ensure Book model is imported in admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Register the Book model
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # columns to display in list view
+    search_fields = ('title', 'author')  # add search capability
+    list_filter = ('publication_year',)  # add filter options
+
