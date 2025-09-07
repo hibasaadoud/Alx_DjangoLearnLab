4faaef36e761807146602778020ada176a8789b7
hibasaadoud <hibasaadoud235@gmail.com>
2025-09-07 13:29:57 -0700

    Fix views and templates for relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,11 +6,13 @@ from django.views.generic import DetailView
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+
 
 # Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
